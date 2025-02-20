0664958e1f607c82fb7871ed197c06b0285815a6
Naman04-coder <n53375374@gmail.com>
2025-02-20 19:06:15 +0530

    Add files via upload
    These are the assignments of Java Programming in day-wise format

---
+++ day1 assignment/program1.java
@@ -0,0 +1,11 @@
+//write a program which prints the following about at least 5person:
+public class program1 {
+    public static void main(String args[]){
+        System.out.println("NAME MAIL-ID EMPLOYEE-CODE PHONE");
+        System.out.println("Naman naman1170@gmail.com 010 6879564555");
+        System.out.println("Ritik ritik089@gmail.com 011 9077722218");
+        System.out.println("Hemank hemanksharma777@gmail.com 012 7882466555");
+        System.out.println("Rahul rtyagi026@gmail.com 013 8098442210");
+        System.out.println("Ankit  ankit7879@gmail.com 014 8956899100");
+    }
+}
---
+++ day2 assignment/NewInstanceObj.java
@@ -0,0 +1,37 @@
+//write a program to implement multiple objects of a class by using instance method of a class.
+public class NewInstanceObj {
+    public static void main(String[] args) {
+        try {
+            // Creating an object of the outer class NewInstanceObj
+            NewInstanceObj outerClass = new NewInstanceObj();
+            Class<?> c = Class.forName("NewInstanceObj$Student");
+
+            // Creating an instance of Student using reflection
+            Student s = (Student) c.getDeclaredConstructor(NewInstanceObj.class).newInstance(outerClass);
+
+            // Setting values using the constructor
+            s.name = "Aarav";
+            s.roll_no = 21;
+
+            System.out.println("Student Details:");
+            s.display();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    class Student {
+        String name;
+        int roll_no;
+
+        public Student() {
+            this.name = "Default Name";
+            this.roll_no = 0;
+        }
+        void display() {
+            System.out.println("Name: " + name);
+            System.out.println("Roll No: " + roll_no);
+        }
+    }
+}
