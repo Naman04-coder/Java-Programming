d06f274edb47c42c54ee812fd561c4f225c33983
Naman04-coder <n53375374@gmail.com>
2025-03-16 22:56:32 +0530

    Add files via upload

---
+++ day4 assignment/TextArray.java
@@ -0,0 +1,8 @@
+class TextArray {
+    public static void main(String[] args) {
+        String[] names = {"Naman", "Komal", "Gaurav", "Ritik", "Saksham"};
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+    }
+}
